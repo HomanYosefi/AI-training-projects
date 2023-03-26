50366eb892e13184255bce52e611c86db19c9643
Homan Yosefi <122898170+HomanYosefi@users.noreply.github.com>
2023-03-26 14:27:26 +0330

    Add files via upload

---
+++ meeting10/kasr.py
@@ -0,0 +1,20 @@
+class kasr: 
+    def __init__(self, s, m):
+        self.sorat = s
+        self.makhraj = m
+
+    def xarb(self):
+        ...
+
+    def jame(self):
+        ...
+
+    def tafrigh(self):
+        ...
+
+    def taghsim(self):
+        ...
+
+    def sade_kon(self):
+        ...
+
---
+++ meeting10/tarikh.py
@@ -0,0 +1,14 @@
+class Tarikh:
+    def __init__(self, yy, mm, dd):
+        self.year = yy
+        self.mah = mm
+        self.day = dd
+
+    def show(self):
+        ...
+
+    def to_day(self):
+        ...    
+
+    def to_normal(self):
+        ...
